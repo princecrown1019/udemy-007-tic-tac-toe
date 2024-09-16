f36d2b31ac322b7c4c3271b3439a9fcecf213978
princecrown1019 <princecrown1019@gmail.com>
2024-09-16 11:32:04 +0300

    build component structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import Logo from "../public/game-logo.png";
 
 function App() {
-  
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <li>
+          <span className="player">
+            <span className="player-game">Player 1</span>
+            <span className="player-symbol">X</span>
+          </span>
+          <button>Edit</button>
+        </li>
+        <li>
+          <span className="player">
+            <span className="player-game">Player 2</span>
+            <span className="player-symbol">O</span>
+          </span>
+          <button>Edit</button>
+        </li>
+      </ol>
+
+      GAME BORAD
+    </div>
+
+    LOG
+  </main>
 
 }
 
